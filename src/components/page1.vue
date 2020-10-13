4e5c3e244528bd644ef67c44d2982796443ad6b8
funasaka <c501306014@gmail.com>
2020-10-13 22:55:50 +0900

    1st commit vue routerおためし

---
+++ src/components/page1.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <p>
+           Here's page1.
+        </p>
+        <router-link to="/page2">Go to page2</router-link>
+    </div>
+</template>
+
+<script/>
+
+<style/>
---
+++ src/components/page2.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>
+            Here's page2.    
+        </p>
+        <router-link to="/">Go to page1</router-link>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
